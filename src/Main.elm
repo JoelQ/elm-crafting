8d95fcc33c896b7746195b574880a7d24846de6e
Joël Quenneville <joelq@thoughtbot.com>
2016-06-22 18:17:51 -0400

    Add UI to input ingredients
    The user can input ingredients via the UI. Clicking the "Brew" button
    results in a hard-coded result.

--- src/Main.elm
+++ src/Main.elm
@@ -2,21 +2,147 @@ module Main exposing (..)
 
 import Html.App exposing (beginnerProgram)
 import Html exposing (..)
+import Html.Attributes exposing (type')
+import Html.Events exposing (onInput, onClick)
+import String
+
+
+type PotionEffect
+    = Heal Int
+    | Energize Int
+    | Poison Int
+
+
+type Plant
+    = Rosmary
+    | Thyme
+    | Elderberry
+    | MustardSeed
+
+
+type alias Ingredient =
+    ( Plant, Int )
+
+
+brew : Model -> Model
+brew model =
+    { model | result = Just (Heal 5) }
+
+
+type alias Model =
+    { rosemary : Int
+    , thyme : Int
+    , elderberry : Int
+    , mustardSeed : Int
+    , result : Maybe PotionEffect
+    }
+
+
+initialModel : Model
+initialModel =
+    { rosemary = 0
+    , thyme = 0
+    , elderberry = 0
+    , mustardSeed = 0
+    , result = Nothing
+    }
 
 
 main =
     beginnerProgram
-        { model = ""
+        { model = initialModel
         , view = view
         , update = update
         }
 
 
-view : a -> Html a
-view _ =
-    h1 [] [ text "Crafting" ]
+view : Model -> Html Msg
+view model =
+    main' []
+        [ h1 [] [ text "Crafting" ]
+        , addIngredient
+        , button [ onClick Brew ] [ text "Brew this potion!" ]
+        , result model.result
+        ]
 
 
-update : a -> String -> String
-update _ model =
-    model
+result : Maybe PotionEffect -> Html a
+result effect =
+    case effect of
+        Nothing ->
+            p []
+                [ em [] [ text "Nothing to brew" ] ]
+
+        Just effect' ->
+            p []
+                [ strong [] [ text (resultText effect') ] ]
+
+
+resultText : PotionEffect -> String
+resultText effect =
+    case effect of
+        Heal n ->
+            "Heal " ++ (toString n)
+
+        Energize n ->
+            "Energize " ++ (toString n)
+
+        Poison n ->
+            "Poison " ++ (toString n)
+
+
+addIngredient : Html Msg
+addIngredient =
+    div []
+        [ ingredientInput Rosmary
+        , ingredientInput Thyme
+        , ingredientInput Elderberry
+        , ingredientInput MustardSeed
+        ]
+
+
+ingredientInput : Plant -> Html Msg
+ingredientInput plant =
+    let
+        intFromString s =
+            Result.withDefault 1 (String.toInt s)
+    in
+        label []
+            [ text (toString plant)
+            , input
+                [ type' "number"
+                , onInput (\n -> AddIngredient ( plant, intFromString n ))
+                ]
+                []
+            ]
+
+
+type Msg
+    = AddIngredient Ingredient
+    | Brew
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        AddIngredient ingredient ->
+            setIngredient ingredient model
+
+        Brew ->
+            brew model
+
+
+setIngredient : Ingredient -> Model -> Model
+setIngredient ingredient model =
+    case ingredient of
+        ( Rosmary, n ) ->
+            { model | rosemary = n }
+
+        ( Thyme, n ) ->
+            { model | thyme = n }
+
+        ( Elderberry, n ) ->
+            { model | elderberry = n }
+
+        ( MustardSeed, n ) ->
+            { model | mustardSeed = n }
